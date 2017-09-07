cbbef58d9b89e58a97eb3c62105283f22539d2bf
millercser <miller.cser@gmail.com>
2017-09-07 15:12:22 +0800

    Initial commit

---
+++ src/main/scala/com/hpe/maven06/App.scala
@@ -0,0 +1,13 @@
+package com.hpe.maven06
+
+import scala.collection.mutable
+
+/**
+ * Hello world!
+ *
+ */
+object App {
+  def main(args: Array[String]): Unit = {
+    println( "Hello World!" )
+  }
+}
